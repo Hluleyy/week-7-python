383b60ae56825f31aeb81e618d9fd4041140b7a6
Hlulani <hluleyy@gmail.com>
2025-07-07 19:40:38 +0200

    week 7 python1

---
+++ strings2.py
@@ -0,0 +1,10 @@
+#Advanced concepts - strings
+
+messege = ' Hello, world '
+
+print(messege.strip())  # removes leading and trailing spaces 
+print(messege.lower())  # converts to all lowercase
+print(messege.split(','))  # Split he string into a list based on the comma
+
+#upper method
+#replace method
